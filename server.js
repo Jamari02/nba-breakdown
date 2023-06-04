03bd1f8f3d96eca1a5cd4f5be65f7de1f21fb5a3
Jamari02 <castrojamari@gmail.com>
2023-06-04 12:30:43 -0400

    Work

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import teamRoutes from '../nba-breakdown/routes/team-routes.js'
-import playerRoutes from '../nba-breakdown/routes/player-routes.js'
-import standingRoutes from '../nba-breakdown/routes/standing-routes.js'
+import teamRoutes from '../routes/team-routes.js'
+import playerRoutes from '../routes/player-routes.js'
+import standingRoutes from '../routes/standing-routes.js'
 
 
 const app = express()
